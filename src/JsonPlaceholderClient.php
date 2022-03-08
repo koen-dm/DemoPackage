c63d66fdf49bb1d6c1c42aec4f60f74d68776125
koendebruijn <bruijnk@gmail.com>
2022-03-08 20:40:28 +0100

    Initial commit

---
+++ src/JsonPlaceholderClient.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JsonPlaceholderClient;
+
+class JsonPlaceholderClient
+{
+    public function getTodo($todoId) {
+        return $this->sendRequest("todos/$todoId");
+    }
+
+    public function getAllTodos() {
+        return $this->sendRequest("todos");
+
+    }
+
+    private function sendRequest($method)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://jsonplaceholder.typicode.com/$method");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+
+        curl_close($ch);
+
+        return json_decode($result);
+    }
+}
